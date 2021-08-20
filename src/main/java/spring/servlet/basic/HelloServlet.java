b0882895a7c9a8e9363bb22b8e6989bee5de686c
hikarigin99 <hikarigin99@gmail.com>
2021-08-20 10:51:33 +0900

    서블릿 생성

---
+++ src/main/java/spring/servlet/basic/HelloServlet.java
@@ -0,0 +1,27 @@
+package spring.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");  // 단순 문자
+        response.setCharacterEncoding("utf-8"); // 메시지 인코딩(한글 허용), httpHeader
+        response.getWriter().write("hello " + username); // httpBody에 데이터 들어감
+    }
+}
