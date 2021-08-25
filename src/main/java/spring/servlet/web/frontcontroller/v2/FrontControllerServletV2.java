b9233b7c605e757b3962581be80d6736b828d1e7
hikarigin99 <hikarigin99@gmail.com>
2021-08-25 16:15:55 +0900

    프론트 컨트롤러 v2 생성

---
+++ src/main/java/spring/servlet/web/frontcontroller/v2/FrontControllerServletV2.java
@@ -0,0 +1,41 @@
+package spring.servlet.web.frontcontroller.v2;
+
+import spring.servlet.web.frontcontroller.MyView;
+import spring.servlet.web.frontcontroller.v2.controller.MemberFormControllerV2;
+import spring.servlet.web.frontcontroller.v2.controller.MemberListControllerV2;
+import spring.servlet.web.frontcontroller.v2.controller.MemberSaveControllerV2;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "frontControllerServletV2", urlPatterns = "/front-controller/v2/*")
+public class FrontControllerServletV2 extends HttpServlet {
+
+    private Map<String, ControllerV2> controllerMap = new HashMap<>();
+
+    public FrontControllerServletV2() {
+        controllerMap.put("/front-controller/v2/members/new-form", new MemberFormControllerV2());
+        controllerMap.put("/front-controller/v2/members/save", new MemberSaveControllerV2());
+        controllerMap.put("/front-controller/v2/members", new MemberListControllerV2());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String requestURI = request.getRequestURI();
+
+        ControllerV2 controller = controllerMap.get(requestURI);    // 다형성 부모 - 자식 (ControllerV1 .. = MemberListControllerV1)
+        if(controller == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);   // 404 오류
+            return;
+        }
+        MyView view = controller.process(request, response);// 다형성으로 인해 오버라이드 된 process 메서드 호출
+        view.render(request, response);
+    }
+}
