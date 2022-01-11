4096ad26f7b0a3ffdcb17415d762856a007079c0
hikarigin99 <hikarigin99@gmail.com>
2022-01-11 15:50:48 +0900

    HTTP Response API HTML, JSON

---
+++ servlet/src/main/java/mvc1/servlet/basic/response/ResponseJsonServlet.java
@@ -0,0 +1,31 @@
+package mvc1.servlet.basic.response;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import mvc1.servlet.basic.HelloData;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+
+        HelloData helloData = new HelloData();
+        helloData.setUsername("kim");
+        helloData.setAge(20);
+
+        // {"username": "kim", "age": 20}
+        String result = objectMapper.writeValueAsString(helloData);// 객체를 써서 문자로 바꿈
+        response.getWriter().write(result);
+    }
+}
