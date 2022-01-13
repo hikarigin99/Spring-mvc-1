0738c9e0a52b50e88709e9e4bf7d4aa9148a75ae
hikarigin99 <hikarigin99@gmail.com>
2022-01-13 15:07:48 +0900

    v3: render메서드 model 파라미터 추가

--- servlet/src/main/java/mvc1/servlet/web/frontcontroller/MyView.java
+++ servlet/src/main/java/mvc1/servlet/web/frontcontroller/MyView.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 public class MyView {
 
@@ -17,5 +18,15 @@ public class MyView {
     public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
+    }
+
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        modelToRequestAttribute(model, request);    //jsp가 request를 사용하기 때문
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+
+    private void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
+        model.forEach((key, value) -> request.setAttribute(key, value));    // model Map에 담겨있는 데이터 모두 접근가능하도록 반복
     }
 }
